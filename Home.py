f8b928f999883ac24a60f079ae422b49ef1d3b2c
dike-okayama <lawliet.d.k.m@gmail.com>
2023-05-31 00:42:08 +0900

    update home and about

--- Home.py
+++ Home.py
@@ -1,3 +1,30 @@
+import time
+import random
 import streamlit as st
 
-st.title("Welcome My Blog")
+bar = st.progress(0, text='waiting')
+
+for i in range(100):
+    # bar.progress(i + 1, text='waiting' + '.' * (i // 10 % 3))
+    # time.sleep(random.random() / (i + 1))
+    pass
+else:
+    bar.empty()
+    st.title('Welcome to My Blog')
+
+    st.write('\n')
+    st.write('\n')
+
+    st.write('''
+    このサイトでは，Pythonの入門書を読み終えた方々がさらに深くPythonを学べるよう，9つのトピックについて断片的に紹介します。
+    それぞれのトピックは初心者が次のステップへ進むために重要でありながらも，魅力的で興味が尽きない内容です．
+
+    ただし，このサイトの情報は[Python公式ドキュメント](python.org)のような総合的なリソースに比べて包括的ではなく，また，部分的に説明が不十分な箇所も存在します．
+    ですので，特定のトピックに興味が湧いた場合は，公式ドキュメントや書籍，他のブログ等を積極的に参照していただくことをお勧めします．
+
+    このサイトが皆様のプログラミングでの表現力を広げる機会になることを願って．
+    ''')
+
+    st.write('<p style="text-align:right;">筆者</p>', unsafe_allow_html=True)
+
+    st.balloons()
---
+++ pages/00_About.py
@@ -0,0 +1,34 @@
+import streamlit as st
+
+st.title('サイトに関して')
+
+st.write('''
+本サイトはPythonの情報提供を目的として作成され，特に意見や新規性を主張するものではありません．
+
+本サイトは筆者がPythonを学ぶ過程で得た知識を整理するために作成されました．
+
+主に以下の書籍やブログに影響を受けています．
+
+- [Python公式ドキュメント](https://docs.python.org/3/)
+- [Pythonチュートリアル](https://docs.python.org/3/tutorial/index.html)
+- [Python標準ライブラリ](https://docs.python.org/3/library/index.html)
+- [Fluent Python]()
+- [Effective Python]()
+- [入門 Python3]()
+- [Python クックブック]()
+- [ハイパフォーマンスPython]()
+- [Python データサイエンスハンドブック]()
+- [独学プログラマー]()
+
+特にFluent Pythonは本サイトの構成に大きな影響を与えています．原著であればWeb上で公開されているため，是非ご一読ください．
+
+本サイトの情報はPythonの公式ドキュメントのような総合的なリソースに比べて包括的ではなく，また，部分的に説明が不十分な箇所も存在します．
+
+なるべく公式ドキュメントからの出典を明記するように努めますが，必ずしも正確な情報を提供できない点をご了承ください．
+''')
+
+st.markdown('---')
+
+st.write('''
+掲載された情報に関するご質問，またお手数ですがご訂正は[こちら](xxx@gmail.com)までご連絡ください．
+''')
